73e0fd41730b9a687d337dd0bdcafef6ecdabb3c
goofiw <goofiw@gmail.com>
2015-07-09 18:02:12 -0700

    connected mongolab

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var jobModel = require('./models/Job')
 
 var app = express();
 
@@ -7,8 +9,23 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/jobs', function(req, res) {
+  mongoose.model('Job').find({}).exec(function(err, collection) {
+    res.send(collection); 
+  })
+});
+
 app.get('*', function(req, res) {
   res.render('index');
 });
 
-app.listen(process.env.PORT || 8080);
+mongoose.connect('mongodb://admin:pack3rs@ds047762.mongolab.com:47762/job-listing');
+
+var con = mongoose.connection;
+
+con.once('open', function(){
+  console.log('mongoose connected');
+  jobModel.seedJobs();
+});
+
+app.listen(process.env.PORT || 3000);
